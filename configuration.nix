490632388a5ba6044dae49a82415083c4ff4e874
Alejandro Garzon <alejitogarbo@gmail.com>
2026-05-08 22:38:15 -0400

    finished adding necessary programs

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
@@ -10,28 +6,12 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelModules = [ "asus-nb-wmi" "amdgpu" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.kernelParams = [
-  #  "amdgpu.runpm=0"
-  #  "processor.max_cstate=1"
-  #  "pcie_aspm=off"
-  #  "idle=nomwait"
-  #  "usbcore.autosuspend=-1"
-  #  "usbcore.quirks=0b05:19b6:k"
-  #  "amdgpu.dpm=0"
-  #  "acpi_backlight=vendor"
-  # ];
-  boot.kernelModules = [ "asus-nb-wmi" "amdgpu" ]; 
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -67,12 +47,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   services.asusd = {
@@ -81,10 +55,7 @@
   };
   services.supergfxd.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User account
   users.users.alej-garz = {
     isNormalUser = true;
     description = "Alejandro Garzon";
@@ -98,40 +69,30 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Install Steam and Gamescope
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     helix
     git
     zed-editor
     xclip
+    element-desktop
+    fzf
+    htop
+    nvtopPackages.amd
+    ghostty
+    mangohud
+    ryzenadj
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-unwrapped"
+  ];
+  
+  system.stateVersion = "25.11"; 
 }

bbf5bb1ad2e9787cc4fbcd0a1489c56f8a3739ff
Alejandro Garzon <alejitogarbo@gmail.com>
2026-05-10 20:59:57 -0400

    added home-manager on the flake file

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
 
     kwin-effects-better-blur-dx = {
       url = "github:xarblu/kwin-effects-better-blur-dx";
@@ -16,6 +17,13 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.alej-garz = ./home.nix;
+        }
       ];
     };
   };

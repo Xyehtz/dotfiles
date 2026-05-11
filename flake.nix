2e6538c6994449c11bd21057ed26e565f2918756
Alejandro Garzon <alejitogarbo@gmail.com>
2026-05-11 00:09:50 -0400

    refactored the flake file

--- flake.nix
+++ flake.nix
@@ -20,10 +20,13 @@
 
         home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.alej-garz = ./home.nix;
-        }
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.alej-garz = ./home.nix;
+            backupFileExtension = "backup";
+          };
+       }
       ];
     };
   };

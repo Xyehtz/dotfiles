8b6f74f365124fb6cf1cff5dcfeb91dc8485d7b0
Alejandro Garzon <alejitogarbo@gmail.com>
2026-05-31 21:50:08 -0400

    slight refactoring on configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,41 +6,53 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Systemd Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # --------------------------------Asus G14 2022 Specific--------------------------------
   boot.kernelParams = [
   	"pcie_aspm=off"
   	"consoleblank=0"
 	  "amdgpu.dcdebugmask=0x10"
-#	  "amdgpu.sg_display=0"
+	  # "amdgpu.sg_display=0"
 	  "acpi_backlight=vendor"
 	  "usbcore.quirks=0b05:19b6:k"
   ];
-
   boot.kernelModules = [ "asus-nb-wmi" "amdgpu" ];
+
   boot.extraModprobeConfig = ''
-	options amdgpu runpm=0
+	  options amdgpu runpm=0
   '';
 
   powerManagement.cpuFreqGovernor = "performance";
 
+  # Asus Linux
+  services.asusd = {
+	  enable = true;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /etc/asusd 0755 root root -"
+  ];
+
+  # ------------------------------End of Asus G14 Specific-------------------------------
+
   hardware.enableRedistributableFirmware = true;
   hardware.graphics = {
-	enable = true;
-	enable32Bit = true;
+  	enable = true;
+  	enable32Bit = true;
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  # Configure network connections interactively with nmcli or nmtui.
+  # Network
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Timezone
   time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_CA.UTF-8";
 
@@ -55,46 +67,49 @@
   # ly
   services.displayManager.ly.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
   # Enable sound.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-	enable = true;
-  	pulse.enable = true;
-	alsa.enable = true;
-	alsa.support32Bit = true;
-  };
-
-  # Asus Linux
-  services.asusd = {
 	  enable = true;
+  	pulse.enable = true;
+	  alsa.enable = true;
+	  alsa.support32Bit = true;
   };
 
-  systemd.tmpfiles.rules = [
-    "d /etc/asusd 0755 root root -"
+  # User account definition
+  users.users.alej-garz = {
+	  isNormalUser = true;
+	  description = "Alej-Garz";
+ 	  extraGroups = [
+ 	    "networkmanager"
+ 	    "wheel"
+ 	    "video"
+ 	    "input"
+ 	  ];
+  };
+
+  programs.steam = {
+    enable = true;
+  };
+
+  # Basic packages for the system
+  environment.systemPackages = with pkgs; [
+	  helix # Main text editor
+	  git
+	  ghostty
+	  rofi
+	  btop
+	  librewolf
+	  element-desktop
+	  gamescope
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.alej-garz = {
-	isNormalUser = true;
-	description = "Alej-Garz";
- 	extraGroups = [ "networkmanager" "wheel" "video" "input" ]; # Enable ‘sudo’ for the user.
-  };
-
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
- 	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	helix
-	git
-	ghostty
-	rofi
-	btop
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-unwrapped"
+    "steam-original"
+    "steam-run"
   ];
 
   system.stateVersion = "26.05"; # Did you read the comment?

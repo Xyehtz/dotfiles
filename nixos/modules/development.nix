dbe74a4c8b487729ce549043e1652cb0c26ffd60
Alejandro Garzon <alejitogarbo@gmail.com>
2026-06-14 00:03:17 -0400

    added Zed and it's configuration

--- nixos/modules/development.nix
+++ nixos/modules/development.nix
@@ -2,26 +2,37 @@
 
 {
   home.packages = with pkgs; [
-    helix
     git
 
+    # Editors
+    zed-editor
+    helix
+
     # LSPs
-    yaml-language-server         # Nix
-    nil                          # Nix
-    nixpkgs-fmt                  # Nix formatter
-    ltex-ls                      # Markdown
-    marksman                     # Markdown
-    taplo                        # TOML
-    codebook                     # Spellcheck
-    phpactor                     # PHP
-    typescript-language-server   # TypeScript and JavaScript
-    tailwindcss-language-server     # Tailwind
+    yaml-language-server # Nix
+    nil # Nix
+    nixd # Nix
+    nixpkgs-fmt # Nix formatter
+    ltex-ls # Markdown
+    marksman # Markdown
+    taplo # TOML
+    codebook # Spellcheck
+    phpactor # PHP
+    typescript-language-server # TypeScript and JavaScript
+    tailwindcss-language-server # Tailwind
     vscode-langservers-extracted # HTML and CSS
-    superhtml                    # HTML
+    superhtml # HTML
   ];
 
   xdg.configFile."helix" = {
     source = config.lib.file.mkOutOfStoreSymlink "/home/alej-garz/Projects/dotfiles/helix/";
+    recursive = true;
+  };
+
+  # Doing the configurations of Zed this way doens't really work, it is better to put the configurations via Home Manager
+  # TODO - Change this to the actual Zed Home Manager settings
+  xdg.configFile."zed" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/alej-garz/Projects/dotfiles/zed";
     recursive = true;
   };
 

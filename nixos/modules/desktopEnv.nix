aa3bec53cdf876db999cb12cd12b873f45985109
Alejandro Garzon <alejitogarbo@gmail.com>
2026-09-01 21:18:28 -0400

    removed DMS due to high resource usage

--- nixos/modules/desktopEnv.nix
+++ nixos/modules/desktopEnv.nix
@@ -8,40 +8,8 @@
   # File Manager
   programs.yazi.enable = true;
   
-  programs.dank-material-shell = {
-    # Enable DankMaterialShell
-    enable = true;
-    
-    theme = "dark";
-
-    # Specify which gop to use
-    dgop.package = inputs.dgop.packages.${pkgs.system}.default;
-
-    # Systemd service
-    systemd = {
-      enable = true;
-      restartIfChanged = true;
-    };
-
-    # Core fearures
-    enableSystemMonitoring = true; # System monitoring tools
-    enableDynamicTheming = true; # Wallpaper based themes
-    enableAudioWavelength = true; # Audio visualizer
-
-    # Disabled core features
-    enableVPN = false;
-    enableCalendarEvents = false;
-
-    clipboardSettings = {
-      maxHistory = 30;
-      maxEntrySize = 5242880;
-      autoClearDays = 1;
-      clearAtStartup = true;
-      disabled = false;
-      disableHistory = false;
-      disablePersist = true;
-    };
-  };
+  # Waybar
+  programs.waybar.enable = true;
 
   # Other packcages
   home.packages = with pkgs; [

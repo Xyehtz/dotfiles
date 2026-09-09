787fb043aa6aa839610f1f4c2680bcbb7fd2bb12
Alejandro Garzon <alejitogarbo@gmail.com>
2026-09-08 21:31:56 -0400

    updated the flake path on the auto upgrade service

--- nixos/hosts/zephyrus/auto-upgrade.nix
+++ nixos/hosts/zephyrus/auto-upgrade.nix
@@ -2,7 +2,7 @@
   system.autoUpgrade = {
     enable = true;
     dates = "weekly";
-    flake = "/home/alej-garz/Projects/dotfiles/nixos#nixos";
+    flake = "/home/alej-garz/Projects/dotfiles/nixos/hosts/zephyrus#nixos";
     randomizedDelaySec = "15min";
   };
 }

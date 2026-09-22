f98efe1eabdddb9ccc650ff1fe6167ca892ae720
Alejandro Garzon <alejitogarbo@gmail.com>
2026-09-21 23:45:27 -0400

    added keybinds on tmux

--- nixos/hosts/zephyrus/programs.nix
+++ nixos/hosts/zephyrus/programs.nix
@@ -36,6 +36,38 @@
       ];
 
       extraConfig = ''
+        # True color
+        set-option -sa terminal-overrides ",xterm*:Tc"
+        set -g mouse on
+
+        # Set Ctrl+Space as prefix
+        unbind C-b
+        set -g prefix C-Space
+        bind C-Space send-prefix
+
+        # Set Vim keys to change between windows
+        bind -n M-H previous-window
+        bind -n M-L next-window
+
+        # Start indexing at 1
+        set -g base-index 1
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+        set-option -g renumber-windows on
+
+        # Enable vi mode
+        set-window-option -g mode-keys vi
+
+        # Vi keybinds
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+        # Rebind pane binds and open on current working directory
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        # Styles
         set -g status-style "bg=#191724"
         setw -g window-status-format "#[fg=#31748f,bg=#191724]#[fg=#191724,bg=#31748f] #I #[fg=#31748f,bg=#191724]#[fg=#e0def4,bg=#191724] #W "
         setw -g window-status-current-format "#[fg=#f6c177,bg=#191724]#[fg=#191724,bg=#f6c177] #I #[fg=#f6c177,bg=#191724]#[fg=#e0def4,bg=#403d52] #W "

404b701e58b6b90282e809f070cc9f522ac33579
Alejandro Garzon <alejitogarbo@gmail.com>
2026-09-21 23:28:27 -0400

    updated the style of tmux

--- nixos/hosts/zephyrus/programs.nix
+++ nixos/hosts/zephyrus/programs.nix
@@ -20,14 +20,29 @@
       nix-direnv.enable = true;
     };
 
+    # Settings here are based on https://github.com/dreamsofcode-io/tmux/blob/main/tmux.conf
     tmux = {
       enable = true;
+
+      extraConfigBeforePlugins = ''
+        set -g @rose_pine_variant 'main'
+      '';
+
       plugins = with pkgs; [
         tmuxPlugins.sensible
         tmuxPlugins.vim-tmux-navigator
         tmuxPlugins.rose-pine
         tmuxPlugins.yank
       ];
+
+      extraConfig = ''
+        set -g status-style "bg=#191724"
+        setw -g window-status-format "#[fg=#31748f,bg=#191724]#[fg=#191724,bg=#31748f] #I #[fg=#31748f,bg=#191724]#[fg=#e0def4,bg=#191724] #W "
+        setw -g window-status-current-format "#[fg=#f6c177,bg=#191724]#[fg=#191724,bg=#f6c177] #I #[fg=#f6c177,bg=#191724]#[fg=#e0def4,bg=#403d52] #W "
+        setw -g window-status-separator " "
+        set -g status-left ""
+        set -g status-right "#[fg=#eb6f92,bg=#191724]#[fg=#191724,bg=#eb6f92] #{pane_current_command} #[fg=#eb6f92,bg=#191724]#[fg=#9ccfd8,bg=#191724]#[fg=#191724,bg=#9ccfd8] #{session_windows} #[fg=#9ccfd8,bg=#191724]"
+      '';
     };
   };
 

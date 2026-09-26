3f75e3a8dff7c4c184b9ba793103033153f77d46
Alejandro Garzon <alejitogarbo@gmail.com>
2026-09-26 12:33:03 -0400

    added smear cursor on Nvim

--- nixos/hosts/zephyrus/modules/nvf-config.nix
+++ nixos/hosts/zephyrus/modules/nvf-config.nix
@@ -159,6 +159,18 @@
               },
             }'';
         };
+
+        # Cursor animations
+        smear-cursor = {
+          package = pkgs.vimPlugins.smear-cursor-nvim;
+          setup = ''
+            require('smear_cursor').setup({
+              stiffness = 0.8,
+              trailing_stiffness = 0.5,
+              distance_stop_animating = 0.5,
+            })
+          '';
+        };
       };
     };
   };
